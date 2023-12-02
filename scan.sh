5720016991b4bf7d5af077d55cc005a0338c23ea
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-12-02 14:54:21 +0330

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Download the binary file x from the URL
-wget https://raw.githubusercontent.com/yebekhe/warp-scanner/main/warp-android-arm64 -O x
+wget https://raw.githubusercontent.com/yebekhe/warp-scanner/main/warp-android-arm64 -O scan
 
 # Set execute permissions for the file
-chmod +x x
+chmod +x scan
 
 # Run the binary file x
-./x
+./scan
 
 # Delete the file x
-rm x
+rm scan
 
 # Show the contents of result.csv
 cat result.csv
